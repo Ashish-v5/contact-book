2a1b690b45ca8c7bb09df2d9583df48efa12da4e
Evgenii_Efimchik <evgenii_efimchik@epam.com>
2022-02-08 11:28:04 +0300

    Initial Commit

---
+++ src/main/java/com/epam/rd/contactbook/Contact.java
@@ -0,0 +1,42 @@
+package com.epam.rd.contactbook;
+
+public class Contact {
+
+    public Contact(String contactName) {
+        //Implement this method
+    }
+
+    public void rename(String newName) {
+        //Implement this method
+    }
+
+    public Email addEmail(String localPart, String domain) {
+        //Implement this method
+    }
+
+
+    public Email addEpamEmail(String firstname, String lastname) {
+        //Implement this method
+    }
+
+    public ContactInfo addPhoneNumber(int code, String number) {
+        //Implement this method
+    }
+
+    public Social addTwitter(String twitterId) {
+        //Implement this method
+    }
+
+    public Social addInstagram(String instagramId) {
+        //Implement this method
+    }
+
+    public Social addSocialMedia(String title, String id) {
+        //Implement this method
+    }
+
+    public ContactInfo[] getInfo() {
+        //Implement this method
+    }
+
+}
